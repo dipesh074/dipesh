546705c143b416d77cd675b0feb183df79db4763
dipesh074 <110055195+dipesh074@users.noreply.github.com>
2022-11-24 10:07:54 +0545

    Variables.php

---
+++ variable.php
@@ -0,0 +1,7 @@
+<?php 
+$i=10;
+$y=5;
+echo "The sum of $i and $y is " . $i + $y . "<br>" ; 
+echo "The difference of $i and $y is " . $i - $y . "<br>" ; 
+echo "The multiply of $i and $y is " . $i * $y  ;
+?>
