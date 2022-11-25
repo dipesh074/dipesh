3d68754cbe279e2e24d6ab25a28c6bd8cd817baf
dipesh074 <110055195+dipesh074@users.noreply.github.com>
2022-11-25 09:57:10 +0545

    switch.php

---
+++ switch.php
@@ -0,0 +1,26 @@
+<?php
+$num = 5;
+switch ($num){
+    case 1:
+        echo "Sorry! Try again";
+        break;
+    case 2:
+        echo "Better luck next time";
+        break;
+    case 3:
+        echo "You Won!";
+        break;
+    case 4:
+        echo "Sorry! Try again";
+         break;
+    case 5:
+        echo "Better luck next time";
+        break;
+    case 6:
+        echo "You Won!";
+        break;
+    default;
+    echo "Your are not selected";
+
+ }
+?>
